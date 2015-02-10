9dad16b749a694525178b1c27faeca435184dcb8
Mads Mætzke Tandrup <mads@maetzke-tandrup.dk>
2015-02-10 09:37:28 +0100

    Add package names to angular

--- src/main/scala/com/wordnik/swagger/codegen/BasicTypeScriptAngularClient.scala
+++ src/main/scala/com/wordnik/swagger/codegen/BasicTypeScriptAngularClient.scala
@@ -10,4 +10,10 @@ class BasicTypeScriptAngularGenerator extends BasicTypeScriptGenerator {
   override def templateDir = "typescript-angular"
 
   override def destinationDir = "generated-code/typescript-angular/"
+
+  // package for models
+  override def modelPackage: Option[String] = Some("dco_api")
+
+  // package for api classes
+  override def apiPackage: Option[String] = Some("dco_api")
 }
